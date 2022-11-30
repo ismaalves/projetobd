dcae18b2208ec42dd0841e14c41bdc7e29cb611b
Ismael Alves Lima <ismael87807394@gmail.com>
2022-11-29 21:54:32 -0300

    Adiconando a rota de ingressos, fazendo filtro de sessoes, e join para pegar o horario das sessoes

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from app import app, db
-from models2 import Filme, Sessao
+from models2 import Filme, Sessao, Hora
 from flask import render_template
+from sqlalchemy import func
 
 @app.route("/")
 def index():
@@ -11,4 +12,19 @@ def index():
 def sessoes(idFilme):
      sessao_filme = Sessao.query.filter_by(fkFilme=idFilme)
      filme = Filme.query.filter_by(idFilme=idFilme).first()
-     return render_template('sessoes.html', sessao_filme=sessao_filme, filme=filme)
+
+     query =f'SELECT "Sessao"."idSessao", "Sessao".experiencia, \
+          "Sessao".formato, "Sessao".idioma, "Sessao"."fkSala",\
+          "Sessao"."fkFilme", "Sessao".dia, "Sessao".valor,\
+          "Sessao".disponibilidade, "hora".horario \
+          FROM "Sessao" JOIN hora ON "Sessao"."fkHora" = hora."idHorario"\
+          WHERE "Sessao"."fkFilme" = {idFilme}'
+
+     sessoes = db.session.execute(query)
+     return render_template('sessoes.html', sessao_filme=sessoes, filme=filme, Hora=Hora)
+
+@app.route("/compraringresso/<int:idSessao>")
+def comprarIngresso(idSessao):
+     sessao = Sessao.query.filter_by(idSessao=idSessao).first()
+
+     return render_template('ingresso.html')
