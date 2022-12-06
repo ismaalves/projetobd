a0f37854d9aebefe9c9124a994034c4e5c493530
Vladimir Yuri <71036663+vladimired12@users.noreply.github.com>
2022-12-05 22:18:33 -0300

    add new forms

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField, StringField, SelectField, FloatField, DateField, validators
+from wtforms.validators import DataRequired
 from models2 import Cliente, Venda
 
 
@@ -25,3 +26,38 @@ class FormVenda(FlaskForm):
 class FormBusca(FlaskForm):
     searched = StringField("Pesquisar", [validators.DataRequired('Invalido')])
     submit = SubmitField('Buscar')
+    
+class cadastrofilmeForm(FlaskForm):
+    
+    nome = StringField("Nome", validators=[DataRequired()])
+
+    classificacao = IntegerField("Classificacao", validators=[DataRequired()])
+    duracao = IntegerField("Duracao", validators=[DataRequired()])
+    ano = IntegerField("Ano", validators=[DataRequired()])
+    
+    sinopse = StringField("Sinopse", validators=[DataRequired()])
+    nacionalidade = SelectField(u"Nacionalidade", choices = [('Nacional'), ('Estrangeiro')], validators=[DataRequired()])
+    produtor = StringField("Produtor", validators=[DataRequired()])
+    diretor = StringField("Diretor", validators=[DataRequired()])
+
+    genero = SelectField(u"Gênero (1-Comédia 2-Romance 3-Ação 4-Aventura 5-Fantasia 6-Terror )",choices = [(1), (2), (3), (4), (5), (6)], validators=[DataRequired()])
+    atores = StringField("Atores", validators=[DataRequired()])
+    img = StringField("Imagem", validators=[DataRequired()])
+    
+    cadastrar = SubmitField('Cadastrar')
+
+class cadastrosessaoForm(FlaskForm):
+    
+    experiencia = SelectField("Experiência", choices=[('VIP'), ('IMAX'), ('MACRO EX')], validators=[DataRequired()])
+    formato = SelectField("Formato", choices=[('2D'), ('3D')], validators=[DataRequired()])
+    idioma = SelectField("Idioma", choices=[('LEGENDADO'), ('DUBLADO')], validators=[DataRequired()])
+
+    salas = SelectField('Sala',choices=[], validators=[DataRequired()])
+    filmes = SelectField('Filme',choices=[], validators=[DataRequired()])
+    horarios = SelectField('Horario',choices=[], validators=[DataRequired()])
+
+    dia = DateField('Data', validators=[DataRequired()])
+    valor = IntegerField('Valor', validators=[DataRequired()])
+    disponibilidade = IntegerField('Disponibilidade', validators=[DataRequired()])
+
+    cadastrar2 = SubmitField('Cadastrar')
